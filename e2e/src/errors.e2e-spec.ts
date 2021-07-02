7b60ae8c07e87f4cf1e713211bf10bb5acf41c36
Spencer Dent <sd00070@my.westga.edu>
2021-07-02 15:58:01 -0400

    abstract & condense verbose contestant input

--- e2e/src/errors.e2e-spec.ts
+++ e2e/src/errors.e2e-spec.ts
@@ -16,7 +16,11 @@ describe('errors', () => {
 
         const clickRegisterButton = () => element(by.id('register-contestants-button')).click()
 
-        const getContestantTextInputs = () => element.all(by.className('contestant-text-input'))
+        const sendContestantTextInputs = (...inputs: string[]): void => {
+            const contestantTextInputs = element.all(by.className('contestant-text-input'))
+
+            inputs.forEach((value: string, index: number) => contestantTextInputs.get(index).sendKeys(value))
+        }
 
         it('displays error when no contestants filled on submit', () => {
             navigateToRegistration()
@@ -29,9 +33,7 @@ describe('errors', () => {
         it('displays an error when 1 player is submitted', () => {
             navigateToRegistration()
 
-            const contestantTextInput = element(by.id('contestant0'))
-
-            contestantTextInput.sendKeys('Samurai Jack')
+            sendContestantTextInputs('Samurai Jack')
 
             clickRegisterButton()
 
@@ -41,11 +43,11 @@ describe('errors', () => {
         it('displays an error when 3 players are submitted', () => {
             navigateToRegistration()
 
-            const contestantTextInputs = getContestantTextInputs()
-
-            contestantTextInputs.get(0).sendKeys('Ed')
-            contestantTextInputs.get(2).sendKeys('Edd')
-            contestantTextInputs.get(3).sendKeys('Eddy')
+            sendContestantTextInputs(
+                'Ed',
+                'Edd',
+                'Eddy'
+            )
 
             clickRegisterButton()
 
@@ -55,13 +57,7 @@ describe('errors', () => {
         it('displays an error when 5 players are submitted', () => {
             navigateToRegistration()
 
-            const contestantTextInputs = getContestantTextInputs()
-
-            contestantTextInputs.get(0).sendKeys('Beast Boy')
-            contestantTextInputs.get(1).sendKeys('Cyborg')
-            contestantTextInputs.get(2).sendKeys('Raven')
-            contestantTextInputs.get(3).sendKeys('Robin')
-            contestantTextInputs.get(4).sendKeys('Starfire')
+            sendContestantTextInputs('Beast Boy', 'Cyborg', 'Raven', 'Robin', 'Starfire')
 
             clickRegisterButton()
 
@@ -71,14 +67,7 @@ describe('errors', () => {
         it('displays an error when 6 players are submitted', () => {
             navigateToRegistration()
 
-            const contestantTextInputs = getContestantTextInputs()
-
-            contestantTextInputs.get(0).sendKeys('Ulrich')
-            contestantTextInputs.get(1).sendKeys('Yumi')
-            contestantTextInputs.get(2).sendKeys('Jeremie')
-            contestantTextInputs.get(3).sendKeys('Odd')
-            contestantTextInputs.get(4).sendKeys('Aelita')
-            contestantTextInputs.get(5).sendKeys('William')
+            sendContestantTextInputs('Ulrich', 'Yumi', 'Jeremie', 'Odd', 'Aelita', 'William')
 
             clickRegisterButton()
 
