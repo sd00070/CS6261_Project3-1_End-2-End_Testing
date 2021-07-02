3f13c45a388485d0bd9e3b1b9d3757e316183deb
Spencer Dent <sd00070@my.westga.edu>
2021-07-02 13:59:09 -0400

    create empty tests to serve as scaffolding

---
+++ e2e/src/errors.e2e-spec.ts
@@ -0,0 +1,29 @@
+describe('errors', () => {
+    describe('registration page', () => {
+        xit('displays error when no contestants filled on submit', () => {
+
+        })
+
+        xit('displays an error when 1, 3, or 5-7 players are submitted', () => {
+
+        })
+
+        xit('displays an error when given duplicate names', () => {
+
+        })
+    })
+
+    describe('tournament page', () => {
+        xit('displays an error if no winners are selected on submission', () => {
+
+        })
+
+        xit('displays an error if any match is missing a selected winner', () => {
+
+        })
+
+        xit('does not increment the counter on error', () => {
+
+        })
+    })
+})
---
+++ e2e/src/general-usage.e2e-spec.ts
@@ -0,0 +1,80 @@
+describe('registering players', () => {
+    xit('registers 2 players & displays them in a message', () => {
+
+    })
+
+    xit('registers 4 players & displays them in a message', () => {
+
+    })
+
+    xit('registers 8 players & displays them in a message', () => {
+
+    })
+
+    xit('displays 1 match on the brackets page after registering 2 players', () => {
+
+    })
+
+    xit('displays 2 matches on the brackets page after registering 4 players', () => {
+
+    })
+
+    xit('displays 4 matches on the brackets page after registering 8 players', () => {
+
+    })
+
+    xit('auto-generates 2 players & registers them', () => {
+
+    })
+
+    xit('auto-generates 4 players & registers them', () => {
+
+    })
+
+    xit('auto-generates 8 players & registers them', () => {
+
+    })
+})
+
+/**
+ * This technically is a start-to-finish test group.
+ * I would test this in isolation, but I am not allowed
+ * to directly interact with the Angular models.
+ */
+describe('running tournaments', () => {
+    xit('increments the round counter when the `complete round` button is clicked while brackets are empty', () => {
+
+    })
+
+    xit('declares a winner when a match winner is selected with only one match (2 players)', () => {
+
+    })
+
+    xit('displays a new round of 1 match when winners for 2 matches (4 players) are submitted', () => {
+
+    })
+
+    xit('displays a new round of 2 matches when winners of 4 matches (8 players) are submitted', () => {
+
+    })
+
+    xit('increments the counter when progressing to another round', () => {
+
+    })
+
+    xit('walks through a full 4-player tournament', () => {
+
+    })
+
+    xit('walks through a full 8-player tournament', () => {
+
+    })
+
+    xit('walks through multiple tournaments', () => {
+
+    })
+
+    xit('remembers registered players when navigating away and to brackets page (staying on site)', () => {
+
+    })
+})
