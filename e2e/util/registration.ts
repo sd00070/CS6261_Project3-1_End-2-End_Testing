e01eeba5ee035319b81265535e16f205f1f1d679
Spencer Dent <sd00070@my.westga.edu>
2021-07-06 12:12:18 -0400

    move methods to appropriate file

--- e2e/util/registration.ts
+++ e2e/util/registration.ts
@@ -1,5 +1,4 @@
-import { by, element, promise } from "protractor"
-import { getContestantTextInputValues } from "./form-interaction"
+import { by, element, ElementArrayFinder, promise } from "protractor"
 import { navigateToRegistration } from "./navigation"
 
 export const clickAutoFill2PlayersButton = () => {
@@ -37,6 +36,18 @@ export const register8Players = () => {
 
     clickRegisterButton()
 }
+
+export const getContestantTextInputs = (): ElementArrayFinder =>
+    element.all(by.className('contestant-text-input'))
+
+export const fillContestantTextInputsWith = (...inputs: string[]): void => {
+    const contestantTextInputs = getContestantTextInputs()
+
+    inputs.forEach((value: string, index: number) => contestantTextInputs.get(index).sendKeys(value))
+}
+
+export const getContestantTextInputValues = (): promise.Promise<string[]> =>
+    getContestantTextInputs().map(element => element.getAttribute('value'))
 
 export const expectContestantTextInputValuesToEqual = (expectedValues: string[]): promise.Promise<void> =>
     getContestantTextInputValues().then(values => expect(values).toEqual(expectedValues))
